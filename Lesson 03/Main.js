6467ce648d73cd027283d952120f178f5375f5a3
eng jumhuur <jumhuur123@hotmail.com>
2024-12-13 22:20:19 +0300

    update Lesson week 02

--- Lesson 03/Main.js
+++
@@ -1,7 +0,0 @@
-console.log(12);
-console.log(50);
-console.log(90);
-
-console.info(1); // console hada waxa uu daabacayaa warbixin
-console.error(2); // console hada waxa uu daabacayaa qalad
-console.warn(3); // console hada waxa uu daabacayaa digniin
--- Main.js
+++ Main.js
@@ -1,17 +1,1 @@
-const User1Age = 16;
-const User1Id = false;
-const User1IsStudent = true;
-var XaqiijintaadaUser1;
-console.log(XaqiijintaadaUser1);
 
-const User2Age = 17;
-const User2Id = false;
-const User2IsStudent = false;
-var XaqiijintaadaUser2;
-console.log(XaqiijintaadaUser2);
-
-const User3Age = 18;
-const User3Id = true;
-const User3IsStudent = false;
-var XaqiijintaadaUser3;
-console.log(XaqiijintaadaUser3);
