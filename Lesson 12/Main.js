f3097c5d73647e0c5a043484a8549d55685165d9
eng jumhuur <jumhuur123@hotmail.com>
2024-12-13 22:15:35 +0300

    done lesson Week 02

---
+++ Lesson 12/Main.js
@@ -0,0 +1,48 @@
+// !=
+
+console.log(5 != "5"); // false
+
+// tusaale 2
+const Price1 = 15; // number
+const Price2 = 15; // number
+console.log(Price1 != Price2); // false
+
+// tusaale 3
+const result1 = 18;
+const result2 = "10";
+console.log(result1 != result2); // true
+
+// !==
+console.log(5 !== "5"); // true
+
+console.log(10 !== 10); // false
+
+// tusaale 2
+
+const Price3 = 15;
+const Price4 = 15;
+console.log(Price3 !== Price4); // false
+
+// ka wayn >
+console.log(5 > 8); // false
+console.log(9 > 9); // false
+console.log(7 > 4); // true
+console.log(1 > 0); // true
+
+// ka yar <
+console.log(5 < 8); // true
+console.log(9 < 9); // false
+console.log(7 < 4); // false
+console.log(1 < 0); // false
+
+// ka wayn ama lamid  >=
+console.log(6 >= 4); // true
+console.log(4 >= 4); // true
+console.log(8 >= 10); // false
+console.log(89 >= 90); // false
+
+// ka yar ama lamid  <=
+console.log(6 <= 4); // false
+console.log(4 <= 4); // true
+console.log(8 <= 10); // true
+console.log(89 <= 90); // true
