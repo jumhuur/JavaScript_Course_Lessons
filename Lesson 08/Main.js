e51032db67cb8f36136f9d3fe7a6b36324758c96
eng jumhuur <jumhuur123@hotmail.com>
2024-12-13 11:13:37 +0300

    added Lesson 6 - 7  Week 02

---
+++ Lesson 08/Main.js
@@ -0,0 +1,25 @@
+console.log(x);
+//waxaan daabacnay ka hor inta aynaan qeexin waxay ina sinaysaa undefined
+var x = 12;
+console.log(x); // 12
+
+// console.log(y); // Error
+// const y = 13;
+
+// console.log(z); // Error
+// let z = 14;
+
+var Price1 = 10;
+console.log(Price1); // 10
+Price1 = 15;
+console.log(Price1); // 15
+
+let Price2 = 20;
+console.log(Price2); // 20
+Price2 = 25;
+console.log(Price2); // 25
+
+const Price3 = 30;
+console.log(Price3); // 30
+Price3 = 35;
+console.log(Price3); // Error
---
+++ Lesson 10/Main.js
@@ -0,0 +1,75 @@
+// /
+console.log(12 / 3); //4
+console.log(50 / 4); //12.50
+console.log(70 / 12); // 5.83
+
+const TotalNumber = 20;
+const Peaple = 5;
+const Result = TotalNumber / Peaple;
+console.log(Result); // 4
+
+// %
+
+console.log(10 % 3); // 1 3 + 3 + 3= 9
+console.log(10 % 5); // 0
+console.log(5 % 4); // 1
+
+const Total = 10;
+const Peaple1 = 3;
+const Result1 = Total % Peaple1;
+console.log(Result1);
+
+// tusaale kale
+
+const Mushahar = 50;
+const shaqaale = 3;
+const TotalShaaqlaha = Mushahar % shaqaale;
+console.log(TotalShaaqlaha);
+
+// **
+console.log(2 ** 3); // 8   waxay ka dhigantahay sidan  (2 * 2 * 2 = 8)
+console.log(50 ** 4); //  waxay ka dhigantahay sidan (50 * 50 * 50 * 50) = 6,250,000
+
+// ++
+let a = 5;
+console.log(a); // 5
+a++;
+console.log(a);
+a++;
+console.log(a);
+// console.log(a); // 6
+// a++;
+// console.log(a); // 7
+
+let Days = 0;
+Days++; // Equivalent to a = a + 1
+console.log(Days); // 1
+Days++;
+Days++;
+Days++;
+Days++;
+console.log(Days); // 5
+
+// --
+let b = 5;
+console.log(b); // 5
+b--;
+console.log(b); // 4
+b--;
+console.log(b); // 3
+b--;
+console.log(b); // 2
+b--;
+console.log(b); // 1
+
+b--;
+console.log(b); // 0
+
+let Day = 30;
+Day--; // Equivalent to a = a - 1
+console.log(Day); // 29
+Day--;
+Day--;
+Day--;
+Day--;
+console.log(Day); // 25
