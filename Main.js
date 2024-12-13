721124d22d7c0f6d139ebab3fa386f0f256da8d9
eng jumhuur <jumhuur123@hotmail.com>
2024-12-13 11:18:43 +0300

    don conf

--- Main.js
+++ Main.js
@@ -1,7 +1,1 @@
-<<<<<<< HEAD
-const Msg = "Soo dhawaaw";
-console.log("My Message", "Msg");
-console.log("Week 01 change");
-=======
-console.log("change something");
->>>>>>> f36992e4ed5c18a4d9a1f91790b462868f18ab9c
+
