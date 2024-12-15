201e2eebfe41031ef50ca27c3823451c1e475301
eng jumhuur <jumhuur123@hotmail.com>
2024-12-15 14:59:36 +0300

    update main branches

---
+++ Lesson 02/Main.js
@@ -0,0 +1,10 @@
+// halkani waa alert
+alert(12); // alert sheegaya qiimaha 12
+// alert
+
+/*
+Tani waa comment dhowr sadar ah.
+Waxaa loo isticmaalaa sharaxaadda code-ka 
+ama in la qariyo qaybo code ah inta lagu jiro horumarinta.
+*/
+let y = 20;
