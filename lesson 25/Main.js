9f2450e98dd251ee7bea1dfce52acea885c0ff1e
eng jumhuur <jumhuur123@hotmail.com>
2025-01-03 11:25:15 +0300

    done

--- lesson 25/Main.js
+++ lesson 25/Main.js
@@ -6,6 +6,7 @@ console.log(parseFloat(Views).toFixed(3));
 // console.log(Hawl1);
 // console.log(Hawl2); // 55645.998
 
+// Tusaale
 const Views1 = "55645.99809 Views"; // 55645
 console.log(Math.trunc(parseFloat(Views1))); // 55645
 
