32f99ac6518cf4603eb94f2d1cc8c8ac8a77727b
eng jumhuur <jumhuur123@hotmail.com>
2025-01-11 07:06:04 +0300

    week_05

---
+++ Week_05/Lesson 36/Main.js
@@ -0,0 +1,13 @@
+const Students = ["Cali", "Maxamad", "Warfaa", "Khaalid", "Xamse", "Saki"];
+Students[Students.length] = "Casiisa";
+Students[Students.length] = "cimraan";
+console.log(Students.length);
+console.log(Students);
+
+if (Students.length > 5) {
+  console.log("Liiska ardaydu wuu buuxaa");
+} else {
+  console.log("Waad is diwaan gallin kartaa");
+}
+
+// index = 0 -  5  length = 1 - 6
---
+++ Week_05/Lesson 38/Main.js
@@ -0,0 +1,12 @@
+// push()
+const Students = ["Cali", "Maxamad", "Warfaa"];
+Students.push("Caaliya");
+Students.push("miigane");
+Students.push("Hodan");
+Students.push("Caaliya", "Migane", "Hodan");
+
+// unshift()
+Students.unshift("Laki");
+Students.unshift("Taysiir", "Farax", "Hoodo");
+console.log(Students.length);
+console.log(Students);
