2acc21e289159db2808de7c24da4bf532b88d911
eng jumhuur <jumhuur123@hotmail.com>
2025-02-26 20:58:35 +0300

    done

---
+++ Week_01/Muraajaco/Muraajaco.js
@@ -0,0 +1,122 @@
+// Week 01 Lesson 01
+// qaar kamida calaamadaha
+//Curly Braces
+//{}
+
+//Square Brackets
+//[]
+
+//Parentheses
+//()
+
+//Semicolon
+//;
+
+//Colon
+//:
+
+//Comma
+//,
+
+//Dot
+
+//.
+
+//Equal
+
+//=
+
+//Arrow
+
+//=>
+
+// Back-ticks
+//``
+
+// ---------- Week 01 Lesson 02 comments -----------------
+
+// halkani waa alert waa comment sare
+alert(12); // alert sheegaya qiimaha 12
+// alert
+
+/*
+Tani waa comment dhowr sadar ah.
+Waxaa loo isticmaalaa sharaxaadda code-ka 
+ama in la qariyo qaybo code ah inta lagu jiro horumarinta.
+*/
+let y = 20;
+
+// ---------- Week 01 Lesson 03 console -----------------
+console.log(12);
+console.log(50);
+console.log(90);
+
+console.info(1); // console hada waxa uu daabacaya warbixin info
+console.error(2); // console hada waxa uu daabacaya qalad Error
+console.warn(3); // console hada waxa uu daabacaya digniin warn
+
+// ---------- Week 01 Lesson 04 variables -----------------
+// var
+var a = 5;
+var b = 6;
+var c = 7;
+
+var Price = 909;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// let
+let AA = 8;
+let BB = 9;
+let CC = 10;
+
+console.log(x);
+console.log(y);
+console.log(z);
+
+// const
+const AB = 11;
+const AC = 12;
+const AD = 13;
+const Qiimo = 550;
+
+console.log(Qiimo);
+console.log(x);
+console.log(y);
+console.log(z);
+
+var Age = 90;
+let Price = 130;
+const Dherer = 176;
+
+console.log(Age);
+console.log(Price);
+console.log(Dherer);
+
+// ---------- Week 01 Lesson 05 variables -----------------
+//Camel Case
+const productPrice = 90;
+// Snake Case
+const Product_Price = 91;
+
+//Pascal Case
+const ProductPrice = 92;
+
+//Single Words
+const age = 12;
+const Price = 13;
+//const Name = "Maxamad";
+
+// const 1uer = 93; Error
+//const all Users = 90; Error
+
+//const @age  = 100  // Error
+//const #price = 89 // Error
+
+//const let = 1000; Error
+//const const = 900; Error
+
+// we use this
+const User = "maxamad";
