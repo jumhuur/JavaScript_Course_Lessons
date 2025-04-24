ef60df1d2a5c24e28e97047b733c8ced3c84d8cc
eng jumhuur <jumhuur123@hotmail.com>
2025-04-24 21:43:09 +0300

    done mini Project

--- Main.js
+++ Main.js
@@ -1,29 +1,50 @@
-// Exercises 01
-let totop = document.querySelector("div.totop");
-console.log(totop);
-
-window.addEventListener("scroll", () => {
-  if (scrollY >= 350) {
-    totop.classList.add("active");
-  } else {
-    totop.classList.remove("active");
+// elements
+const body = document.querySelector("body");
+const Head = document.querySelector("div.main_head");
+const input = document.querySelector("form input");
+const btn = document.querySelector("form button");
+const lists = document.querySelector("div.list");
+const text = document.querySelector("div.text p");
+//localStorage.setItem("mode", "false");
+const data = navigator.userAgentData;
+const setmode = () => {
+  const SystemMode = matchMedia("(prefers-color-scheme: dark)").matches;
+  const Localmode = localStorage.getItem("mode");
+  console.log(SystemMode);
+  console.log(Localmode);
+  if (Localmode == null) {
+    if (SystemMode === true) {
+      body.classList.add("dark");
+      Head.classList.add("dark");
+      input.classList.add("dark");
+      btn.classList.add("dark");
+      lists.classList.add("dark");
+      text.classList.add("dark");
+    } else {
+      body.classList.remove("dark");
+      Head.classList.remove("dark");
+      input.classList.remove("dark");
+      btn.classList.remove("dark");
+      lists.classList.remove("dark");
+      text.classList.remove("dark");
+    }
   }
-});
 
-totop.addEventListener("click", () => {
-  scrollTo(0, 0);
-});
+  if (Localmode === "true") {
+    body.classList.add("dark");
+    Head.classList.add("dark");
+    input.classList.add("dark");
+    btn.classList.add("dark");
+    lists.classList.add("dark");
+    text.classList.add("dark");
+  } else if (Localmode === "false") {
+    body.classList.remove("dark");
+    Head.classList.remove("dark");
+    input.classList.remove("dark");
+    btn.classList.remove("dark");
+    lists.classList.remove("dark");
+    text.classList.remove("dark");
+  }
+};
 
-// Exercis 02
-const Topbtn = document.querySelector("button.close");
-const buttom = document.getElementById("close");
-
-Topbtn.addEventListener("click", () => {
-  let Scroll = scrollY;
-  scrollTo(0, scrollY - 20);
-});
-
-buttom.addEventListener("click", () => {
-  //let Scroll = scrollY;
-  scrollBy(0, 20);
-});
+setmode();
