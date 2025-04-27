35d98676c7af54cecd38f0d7fc833410c9e65437
eng jumhuur <jumhuur123@hotmail.com>
2025-04-27 12:45:38 +0300

    done

--- Main.js
+++ Main.js
@@ -1,200 +1,95 @@
-// elements
-const body = document.querySelector("body");
-const Head = document.querySelector("div.main_head");
-const input = document.querySelector("section input");
-const btn = document.querySelector("section button");
-//const lists = document.querySelector("div.list");
-//const text = document.querySelector("div.text p");
-const modelist = document.querySelector("div.list_mode");
-const switchermode = document.querySelector("div.mode");
-const allmodes = document.querySelectorAll("ul.drop_mode li");
-const modeicon = document.getElementById("mode_icon");
-const form = document.querySelector("section");
-const listrapper = document.querySelector("div.lists");
-const complatediv = document.querySelector("div.complate > p");
-const uncomplatediv = document.querySelector("div.uncomplate > p");
+// const color = document.querySelector("input#color");
+// const Body = document.querySelector("body");
+// const ul = document.querySelector("ul");
+// const h2 = document.querySelector("h2");
+// const btn = document.querySelector("button");
+// // get colors from localstrg
+// const LastColor = localStorage.getItem("color");
+// const Fullcolors = localStorage.getItem("Allcolors");
+// let colorHistroy = JSON.parse(Fullcolors) || [];
+// // function getContrastColor(hexColor) {
+// //   const color = hexColor.replace("#", "");
+// //   const r = parseInt(color.slice(0, 2), 16);
+// //   const g = parseInt(color.slice(2, 4), 16);
+// //   const b = parseInt(color.slice(4, 6), 16);
+// //   // formula: brightness
+// //   const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+// //   // return white or black depending on brightness
+// //   return brightness > 128 ? "#000000" : "#FFFFFF";
+// // }
+// const Setcolors = () => {
+//   Body.style.backgroundColor = `${LastColor}`;
+//   color.value = LastColor;
+//   colorHistroy.forEach((color, index) => {
+//     const li = document.createElement("li");
+//     li.textContent = `${index + 1} : ${color}`;
+//     ul.appendChild(li);
+//     li.style.backgroundColor = color;
+//     li.style.color = "#fff";
+//   });
+// };
 
-// get all todolists
-const allTodo = localStorage.getItem("todos");
-const Todo_lists = JSON.parse(allTodo) || [];
-//localStorage.setItem("mode", "false");
-const data = navigator.userAgentData;
-// open and close mode list
-switchermode.addEventListener("click", () => {
-  modelist.classList.toggle("active");
+// // changing
+// color.addEventListener("input", () => {
+//   h2.style.color = colorHistroy[2];
+//   Body.style.backgroundColor = `${color.value}`;
+//   colorHistroy.push(color.value);
+//   localStorage.setItem("color", color.value);
+//   localStorage.setItem("Allcolors", JSON.stringify(colorHistroy));
+//   const li = document.createElement("li");
+//   colorHistroy.forEach((color, index) => {
+//     li.textContent = `${index + 1} : ${color}`;
+//     ul.appendChild(li);
+//     li.style.backgroundColor = color;
+//     li.style.color = "#fff";
+//   });
+// });
+// btn.addEventListener("click", () => {
+//   localStorage.clear();
+//   ul.innerHTML = "";
+//   color.value = "";
+//   Body.style.backgroundColor = "#fff";
+// });
+// Setcolors();
+
+// console.log("2e014b".substr(0, 2));
+// console.log("2e014b".substr(2, 2));
+// console.log("2e014b".substr(4, 6));
+// console.log("*".repeat(10));
+// console.log("2e014b".slice(0, 2));
+// console.log("2e014b".slice(2, 4));
+// console.log("2e014b".slice(4, 6));
+// console.log("2e014b");
+// console.log(parseInt("2e014b".slice(0, 2), 16));
+// console.log(parseInt("2e014b".slice(2, 4), 16));
+// console.log(parseInt("2e014b".slice(4, 7), 16));
+
+const sound = document.querySelector("audio");
+const Playbtn = document.querySelector("button");
+const Pausebtn = document.querySelector("button.stop");
+Playbtn.addEventListener("click", () => {
+  setTimeout(() => {
+    sound.play();
+    console.log("done");
+  }, 300);
 });
 
-// private
-
-const setStaticdatat = () => {
-  let complate_text = Todo_lists.filter((todo) => {
-    return todo.complate === true;
-  });
-
-  let uncomplate_text = Todo_lists.filter((todo) => {
-    return todo.complate === false;
-  });
-  complatediv.textContent = `complate : ${complate_text.length}`;
-  uncomplatediv.textContent = `uncomplate : ${uncomplate_text.length}`;
-};
-
-// delete function
-const DeleteTodo = (Id) => {
-  console.log(Id);
-  const saafi = Todo_lists.filter((todo) => {
-    return todo.id != Id;
-  });
-  localStorage.setItem("todos", JSON.stringify(saafi));
-  setdata();
-  console.log(Id);
-};
-
-// update funtions
-const complateTodo = (Id) => {
-  let Todonow = Todo_lists.filter((Todo) => {
-    return Todo.id === Id;
-  });
-  Todonow = [{ ...Todonow[0], complate: true }];
-  console.log(Todonow);
-  let All = Todo_lists.filter((Todo) => {
-    return Todo.id !== Id;
-  });
-
-  All.push(...Todonow);
-  localStorage.setItem("todos", JSON.stringify(All));
-  setdata();
-  console.log(All);
-};
-
-const setmode = () => {
-  const SystemMode = matchMedia("(prefers-color-scheme: dark)").matches;
-  const Localmode = localStorage.getItem("mode");
-  setStaticdatat();
-  console.log(SystemMode);
-  console.log(Localmode);
-  if (Localmode == null) {
-    if (SystemMode === true) {
-      body.classList.add("dark");
-      Head.classList.add("dark");
-      input.classList.add("dark");
-      btn.classList.add("dark");
-      listrapper.classList.add("dark");
-      modeicon.innerHTML = `<i class="bx bx-desktop"></i>`;
-    } else {
-      body.classList.remove("dark");
-      Head.classList.remove("dark");
-      input.classList.remove("dark");
-      btn.classList.remove("dark");
-      listrapper.classList.remove("dark");
-      modeicon.innerHTML = `<i class="bx bx-desktop"></i>`;
-    }
-  }
-
-  if (Localmode === "true") {
-    body.classList.add("dark");
-    Head.classList.add("dark");
-    input.classList.add("dark");
-    btn.classList.add("dark");
-    listrapper.classList.add("dark");
-
-    modeicon.innerHTML = `<i class="bx bxs-moon"></i>`;
-  } else if (Localmode === "false") {
-    body.classList.remove("dark");
-    Head.classList.remove("dark");
-    input.classList.remove("dark");
-    btn.classList.remove("dark");
-    listrapper.classList.remove("dark");
-
-    modeicon.innerHTML = `<i class="bx bxs-sun"></i>`;
-  }
-};
-
-setmode();
-
-// change mode
-allmodes.forEach((mode) => {
-  mode.addEventListener("click", (e) => {
-    const class_now = e.target.className;
-    console.log(class_now);
-    if (class_now === "bx bxs-moon") {
-      localStorage.setItem("mode", "true");
-      modeicon.innerHTML = `<i class="bx bxs-moon"></i>`;
-      setmode();
-    } else if (class_now === "bx bxs-sun") {
-      localStorage.setItem("mode", "false");
-      modeicon.innerHTML = `<i class="bx bxs-sun"></i>`;
-      setmode();
-    } else {
-      localStorage.removeItem("mode", "false");
-      modeicon.innerHTML = `<i class="bx bx-desktop"></i>`;
-      setmode();
-    }
-  });
+Pausebtn.addEventListener("click", () => {
+  setTimeout(() => {
+    sound.pause();
+    console.log("done");
+  }, 300);
 });
-// add todolist
-const setdata = () => {
-  setStaticdatat();
-  listrapper.innerHTML = "";
-  if (Todo_lists.length === 0) {
-    listrapper.innerHTML = `<div class="empty_todo">
-<div class="image_empty">
-  <img src="/Images/Clipboard.png" alt="empty" />
-</div>
-<h2>empty list</h2>
-<p>There are no tasks available</p>
-</div>`;
-  }
 
-  if (Todo_lists.length > 0) {
-    Todo_lists.forEach((todo) => {
-      const sectionList = document.createElement("div");
-      sectionList.classList.add("list");
-      sectionList.innerHTML = `<div class="icon">
-              ${
-                todo.complate === false
-                  ? `<i class="bx bx-circle"></i>`
-                  : `<i class="bx bxs-check-circle"></i>`
-              }
-            </div>
-            <div class="text">
-            ${
-              todo.complate === true
-                ? `<p style="text-decoration: line-through;">
-                ${todo.Title}
-              </p>`
-                : `<p>
-                ${todo.Title}
-              </p>`
-            }
+// properties
+console.log(sound.audioTracks);
+console.log(sound.currentTime);
+console.log(sound.duration);
+console.log(sound.playbackRate);
+console.log(sound.volume);
+console.log(sound.src);
 
-            </div>
-            <div class="action">
-              <i class="bx bx-trash"></i>
-            </div>`;
-      listrapper.appendChild(sectionList);
-    });
-  }
-};
-
-setdata();
-btn.addEventListener("click", (e) => {
-  const Todo = {
-    id: crypto.randomUUID(),
-    Title: input.value,
-    complate: false,
-  };
-
-  if (input.value != "") {
-    Todo_lists.push(Todo);
-    localStorage.setItem("todos", JSON.stringify(Todo_lists));
-    input.value = "";
-    listrapper.innerHTML = "";
-    setdata();
-  } else {
-    alert("you cant add empty todo ");
-  }
-
-  setdata();
+sound.addEventListener("timeupdate", (e) => {
+  console.log("wakhtiga Muuqalka :", e.target.duration);
+  console.log("wakhtiga uu marayo", e.target.currentTime);
 });
-setStaticdatat();
-complateTodo("531014ae-a13b-4b01-bcc9-6eff430d09ae");
