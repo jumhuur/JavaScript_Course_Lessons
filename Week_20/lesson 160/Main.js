c1bb30209f88271c3b720b976e1aa1be08509a7e
jumhuur <jumhuur123@hotmail.com>
2025-08-22 05:58:01 +0300

    small change in week 20 lessons

--- Week_20/lesson 160/Main.js
+++ Week_20/lesson 160/Main.js
@@ -1,11 +1,50 @@
-// post
+// POST METHOD
 
-// const PostObject = {
-//   title: "tile hada aan qornay",
-//   body: "qoraal cusub oo oa kale ",
-//   userId: 25,
-// };
-const Postdata = async () => {
+const PostObject = {
+  title: "tile hada aan qornay",
+  body: "qoraal cusub oo oa kale ",
+  userId: 25,
+};
+
+const Post = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "DELETE",
+    body: JSON.stringify(PostObject),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Errort ${response.status}`);
+  }
+};
+
+Post();
+
+// PUT METHOD
+const UpdatePost = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    body: JSON.stringify(PostObject),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw new Error(`Errort ${response.status}`);
+  }
+};
+
+UpdatePost();
+
+// DELETE METHOD
+const DeletePost = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/1", {
     method: "DELETE",
   });
@@ -17,8 +56,4 @@ const Postdata = async () => {
   }
 };
 
-Postdata();
-
-// put
-
-// delete
+DeletePost();
